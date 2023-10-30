91e70584f3bb48154acc9feb10ebd50d19c2dc05
Ibnu Huygenz <ibnuwdodo@gmail.com>
2023-10-30 22:18:55 +0700

    Initial commit

---
+++ app/src/main/java/pam/tugas/uts_prak_pam/store_detail.kt
@@ -0,0 +1,11 @@
+package pam.tugas.uts_prak_pam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class store_detail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_store_detail)
+    }
+}
